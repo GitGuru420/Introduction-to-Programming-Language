3d301f3172f7d0dc7514770fe57e196652b62c78
GitGuru420 <raisulislam1892@gmail.com>
2024-10-02 01:46:52 +0530

    Nested loop in C

--- Week-1/Module-3/D-Nested-Loop.c
+++ Week-1/Module-3/D-Nested-Loop.c
@@ -7,5 +7,14 @@ int main() {
         printf("\n");
     }
 
+    printf("\n\n");
+
+    for(int p=1; p<3; p++) {
+        for(int q=1; q<5; q++) {
+            printf("%d hour, %d minute\n", p, q);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
