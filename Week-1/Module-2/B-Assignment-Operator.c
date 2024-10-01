d705c596afc02c5fd65ff77c09b82963753af0ed
GitGuru420 <raisulislam1892@gmail.com>
2024-10-01 15:34:20 +0530

    Assignment Operator

---
+++ Week-1/Module-2/B-Assignment-Operator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    printf("%d\n", a);
+
+    // a += 10;
+    // a -= 10;
+    // a *= 10;
+    // a /= 10;
+    // a %= 10;
+    printf("%d\n", a);
+
+    return 0;
+}
