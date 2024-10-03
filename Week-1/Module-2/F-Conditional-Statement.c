2f9a42e7fbbb741575075a4b16e9ccaa7621c1a6
GitGuru420 <raisulislam1892@gmail.com>
2024-10-03 17:53:46 +0530

    Conditional Statements

---
+++ Week-1/Module-2/F-Conditional-Statement.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    // int a = 5;
+    int a = 7;
+    printf("Before if\n");
+    if(a == 5) {
+        printf("Inside if\n");
+    }
+    printf("After if\n");
+
+    return 0;
+}
