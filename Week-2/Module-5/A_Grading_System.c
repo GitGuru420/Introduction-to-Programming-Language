27eb8a15146e3030698a4ea4dc0582421a9618ae
GitGuru420 <raisulislam1892@gmail.com>
2024-10-06 10:41:36 +0530

    Grading System Updated

--- Week-2/Module-5/A_Grading_System.c
+++ Week-2/Module-5/A_Grading_System.c
@@ -4,19 +4,19 @@ int main() {
     scanf("%d", &marks);
 
     if(marks >= 80 && marks <= 100)
-        printf("You have got A+ Grade");
+        printf("You have got A+ Grade.\nYou have passed.");
     else if(marks >= 70 && marks <= 79)
-        printf("You have got A Grade");
+        printf("You have got A Grade.\nYou have passed");
     else if(marks >= 60 && marks <= 69)
-        printf("You have got A- Grade");
+        printf("You have got A- Grade.\nYou have passed");
     else if(marks >= 50 && marks <= 59)
-        printf("You have got B Grade");
+        printf("You have got B Grade.\nYou have passed");
     else if(marks >= 40 && marks <= 49)
-        printf("You have got C Grade");
+        printf("You have got C Grade.\nYou have passed");
     else if(marks >= 33 && marks <= 39)
-        printf("You have got D Grade");
+        printf("You have got D Grade.\nYou have passed");
     else if(marks >= 0 && marks <= 32)
-        printf("You have got F Grade");
+        printf("You have got F Grade.\nYou have failed. Try again.");
     else
         printf("Invalid Marks");
 
