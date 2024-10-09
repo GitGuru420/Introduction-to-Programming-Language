e081364c8626704cfddef3abad22c152c5e6c9c9
GitGuru420 <raisulislam1892@gmail.com>
2024-10-09 15:13:05 +0530

    Declare and initialize array and print array elements

---
+++ Week-2/Module-7/SP_1_Array_Basic.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    // declare and initialize array
+    int arr[5] = {2, 4, 6, 8, 10};
+    
+    // print the array elements
+    for(int i=0; i<5; i++) {
+        printf("%d ", arr[i]);
+    }
+    
+    return 0;
+}
