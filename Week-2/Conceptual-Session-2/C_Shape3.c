728011bb81e4ea3ee334308ff70b3a19c68347a7
GitGuru420 <raisulislam1892@gmail.com>
2024-10-12 12:26:06 +0530

    Diamond Pattern Another Version

--- Week-2/Conceptual-Session-2/C_Shape3.c
+++ Week-2/Conceptual-Session-2/C_Shape3.c
@@ -5,27 +5,33 @@ int main() {
     int space = n-1;
     int star = 1;
     
-    for(int i=1; i<=n; i++) {
+    for(int i=1; i<=2*n; i++) {
         for(int j=1; j<=space; j++)
             printf(" ");
-        space--;
         for(int j=1; j<=star; j++)
             printf("*");
-        star += 2;
         printf("\n");
+        if(i<n) {
+            space--;
+            star += 2;
+        }
+        else if(i>n) {
+            space++;
+            star -= 2;
+        }
     }
 
-    space = 0;
-    star = (2*n) - 1;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=space; j++)
-            printf(" ");
-        space++;
-        for(int j=1; j<=star; j++)
-            printf("*");
-        star -= 2;
-        printf("\n");
-    }
-    
+    // space = 0;
+    // star = (2*n) - 1;
+    // for(int i=1; i<=n; i++) {
+    //     for(int j=1; j<=space; j++)
+    //         printf(" ");
+    //     space++;
+    //     for(int j=1; j<=star; j++)
+    //         printf("*");
+    //     star -= 2;
+    //     printf("\n");
+    // }
+
     return 0;
 }
