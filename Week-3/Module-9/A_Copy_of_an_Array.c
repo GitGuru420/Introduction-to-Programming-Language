de718300859c25ab406cf7c7065ff6c50b18fe81
GitGuru420 <raisulislam1892@gmail.com>
2024-10-22 00:35:29 +0530

    Copy of an array

--- Week-3/Module-9/A_Copy_of_an_Array.c
+++ Week-3/Module-9/A_Copy_of_an_Array.c
@@ -1,37 +1,40 @@
 #include <stdio.h>
 int a[100000], b[100000];
 int main() {
-    int length_a = 0, length_b = 0;
-    scanf("%d", &length_a);
-    for(int i=0; i<length_a; i++) {
+    int length_a1 = 0;
+    scanf("%d", &length_a1);
+
+    for(int i=0; i<length_a1; i++) {
         scanf("%d", &a[i]);
     }
 
-    printf("Befor Copy\n");
-    for(int i=0; i<length_a; i++) {
+    printf("Before Copy: Element of array(a):\n");
+    for(int i=0; i<length_a1; i++) {
         printf("%d ", a[i]);
     }
-
     printf("\n");
-    for(int i=0; i<length_a; i++) {
+    printf("Before Copy: Element of array(b):\n");
+    for(int i=0; i<length_a1; i++) {
         printf("%d ", b[i]);
     }
+    printf("\n");
 
-    for(int i=0; i<length_a; i++) {
+    // copy array element
+    for(int i=0; i<length_a1; i++) {
         b[i] = a[i];
     }
-    length_b = length_a;
+    printf("After Copy: Element of array(b)\n");
+    for(int i=0; i<length_a1; i++) {
+        printf("%d ", b[i]);
+    }
+    printf("\n");
 
-    // update array index value
+    // update array element
+    printf("Update array element\n");
     a[2] = 100;
-    printf("\nAfter Copy\n");
-    for(int i=0; i<length_a; i++) {
+    for(int i=0; i<length_a1; i++) {
         printf("%d ", a[i]);
     }
 
-    printf("\n");
-    for(int i=0; i<length_b; i++) {
-        printf("%d ", b[i]);
-    }
     return 0;
 }
