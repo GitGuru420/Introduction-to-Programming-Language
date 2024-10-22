695e3741ffe1588e447ec2d772ad3ef1eba8b1f8
GitGuru420 <raisulislam1892@gmail.com>
2024-10-22 19:38:16 +0530

    Code for string initialization

---
+++ Week-3/Module-10/Phitron-Book/C_Initialize.c
@@ -0,0 +1,15 @@
+// string initialization
+#include <stdio.h>
+int main() {
+    // char a[5] = {'R', 'a', 'h', 'a', 't'};
+    // char a2[5] = "Rahat";
+
+    // printf("%s", a);
+    // printf("%s", a2);
+
+    // char a3[6] = "Rahat\0";
+    char a3[6] = "Rahat";
+    printf("%s", a3);
+
+    return 0;
+}
