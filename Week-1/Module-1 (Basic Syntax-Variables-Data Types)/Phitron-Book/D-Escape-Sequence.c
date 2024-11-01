9e0e255fdbd0d9c75cd7755014b3ebd332ef853c
GitGuru420 <raisulislam1892@gmail.com>
2024-11-02 01:42:07 +0530

    Escape Sequence

---
+++ Week-1/Module-1 (Basic Syntax-Variables-Data Types)/Phitron-Book/D-Escape-Sequence.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    // new line: \n
+    printf("Hello\nWorld!\n");
+    
+    // horizontal tab: \t
+    printf("Raisul Islam\n");
+    printf("Raisul\tIslam\n");
+
+    // double quote: \" \"
+    printf("\"Bangladesh\" is a beautiful country.\n");
+
+    // single quote: \' \'
+    printf("\'Bangladesh\' is a beautiful country.\n");
+
+    // backslash: 
+    printf("Raisul\\Sharmin\n");
+
+    // percentage: %
+    printf("Give 100%% force.\n");
+
+    return 0;
+}
