a0d9815bb57727d53712216bd3b460073c3434b9
GitGuru420 <raisulislam1892@gmail.com>
2024-11-02 01:45:38 +0530

    Escape Sequence

---
+++ Week-1/Module-1 (Basic Syntax-Variables-Data Types)/Phitron-Book/E-Data-type-Limitation.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int population = 10000000000;
+    printf("Population: %d\n", population);
+
+    long long population2 = 10000000000;
+    printf("Population: %lld", population2);
+
+    return 0;
+}
