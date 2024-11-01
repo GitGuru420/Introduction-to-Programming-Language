088145ae2e70e5b2d96d3b820d49d09fabf547fa
GitGuru420 <raisulislam1892@gmail.com>
2024-11-02 01:29:10 +0530

    Variable & Data types

---
+++ Week-1/Module-1 (Basic Syntax-Variables-Data Types)/Phitron-Book/B-Variable-Data-type.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    // variable declaration 
+    // syntax: data_type variable_name;
+    int age;
+    float pi;
+    char letter;
+
+    // assign variable value
+    // syntax: variable_name = value;
+    age = 23;
+    pi = 3.1416;
+    letter = 'R';
+
+    // accessing variable value
+    printf("Age: %d\n", age);
+    printf("PI: %f\n", pi);
+    printf("Letter: %c\n", letter);
+
+    return 0;
+}
