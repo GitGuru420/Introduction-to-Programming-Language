bbf1ec64d1dda0831201866d87dea52584e73aa7
GitGuru420 <raisulislam1892@gmail.com>
2024-11-04 12:55:32 +0530

    Usefull Built in Functions

---
+++ Week-5 (Functions and Recursion)/Module-17 (Function in C)/E-Built-in-Functions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double a;
+    scanf("%lf", &a);
+
+    printf("%lf\n", ceil(a));
+    printf("%lf\n", floor(a));
+    printf("%lf\n", round(a));
+    printf("%lf\n", sqrt(4));
+    printf("%lf\n", sqrt(8));
+    printf("%lf\n", pow(2,5));
+
+    return 0;
+}
