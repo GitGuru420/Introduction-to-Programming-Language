84c611d3f90a5e34706da61396978101a5d09495
GitGuru420 <raisulislam1892@gmail.com>
2024-11-04 11:40:29 +0530

    Function with return no parameter

---
+++ Week-5 (Functions and Recursion)/Module-17 (Function in C)/B-Return-No-Parameter.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int add() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("Function Called\n");
+    int sum = a + b;
+    return sum;
+}
+
+double getPi() {
+    return 3.14159;
+}
+int main() {
+    printf("Before function calling\n");
+    printf("%d\n", add());
+    printf("%d\n", add());
+    printf("%lf\n", getPi());
+
+    return 0;
+}
