2aef295ae4a7256263a3d99fc342b512ba027341
GitGuru420 <raisulislam1892@gmail.com>
2024-11-05 15:15:21 +0530

    Conditional Statements

---
+++ Week-1/Conceptual-Session-1 (Data-type and Variables)/E-Conditional-Statements.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    if(5 < 3 || 5 > 10) {
+        printf("Ghotona Sotto\n");
+    }
+    else if(3 != 2 && 3 > 0) {
+        printf("Majher Ghotona Sotto\n");
+    }
+    else {
+        printf("Ghotona Mittha\n");
+    }
+
+    return 0;
+}
