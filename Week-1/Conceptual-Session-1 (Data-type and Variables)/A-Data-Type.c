31cd3a7583b1756d6693498674c7a2fae8cebbd7
GitGuru420 <raisulislam1892@gmail.com>
2024-11-05 09:14:03 +0530

    Data Type

---
+++ Week-1/Conceptual-Session-1 (Data-type and Variables)/A-Data-Type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  // preprossesing unit
+#include <stdbool.h>
+int main() {
+    int n = 5;
+    float f = 5.67;
+    int ft = 5.67;
+    char ch = 'a';
+    bool opt = true;
+
+    return 0;
+}
